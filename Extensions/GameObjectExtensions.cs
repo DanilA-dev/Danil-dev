94e01957a373205e3ce71817b748e9b941cfe0b3
DanilA-dev <78906212+DanilA-dev@users.noreply.github.com>
2024-07-30 15:34:05 +0300

    Add files via upload

---
+++ Extensions/GameObjectExtensions.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Extensions
+{
+    public static class GameObjectExtensions
+    {
+        public static bool TryGetComponentInAny<T>(this GameObject gameObject, out T component)
+        {
+            if (gameObject.TryGetComponent(out component))
+                return true;
+
+            var root = gameObject.transform.root;
+            if (root.TryGetComponent(out component))
+                return true;
+
+            var childInRoot = root.GetComponentInChildren<T>();
+            if (childInRoot != null)
+            {
+                component = childInRoot;
+                return true;
+            }
+            
+            return false;
+        }
+    }
+}
