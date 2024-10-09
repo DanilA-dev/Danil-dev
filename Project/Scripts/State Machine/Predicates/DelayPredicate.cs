dc7bba0c5605d10f416dead83654046b7e3bd00f
DanilA-dev <78906212+DanilA-dev@users.noreply.github.com>
2024-10-09 12:24:41 +0300

    Add files via upload

---
+++ Project/Scripts/State Machine/Predicates/DelayPredicate.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace CustomFSM.Preicate
+{
+    public class DelayPredicate : IPredicate
+    {
+        private CountdownTimer _timer;
+
+        public bool CanBeUpdated { get; set; } = true;
+        public DelayPredicate(float time)
+        {
+            _timer = new CountdownTimer(time);
+            _timer.Start();
+        }
+
+
+        public bool Evaluate()
+        {
+            if(!_timer.IsRunning && _timer.RemainingTime <= 0)
+                _timer.Start();
+            
+            _timer.Tick(Time.deltaTime);
+            return CanBeUpdated && !_timer.IsRunning;
+        }
+    }
+}
