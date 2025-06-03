613f56ef361afca442e31443a226abd5e3774b50
DanilA-dev <78906212+DanilA-dev@users.noreply.github.com>
2025-06-03 18:44:23 +0300

    .

--- Scripts/EventHandler/CustomEventType.cs
+++
@@ -1,8 +0,0 @@
-namespace D_dev.Scripts.EventHandler
-{
-    public enum CustomEventType
-    {
-       Event_1 = 0,
-       Event_2 = 1
-    }
-}
---
+++ Scripts/Runtime/UtilScripts/EventHandler/CustomEventType.cs
@@ -0,0 +1,7 @@
+namespace D_dev.Scripts.EventHandler
+{
+    public enum CustomEventType
+    {
+      
+    }
+}
