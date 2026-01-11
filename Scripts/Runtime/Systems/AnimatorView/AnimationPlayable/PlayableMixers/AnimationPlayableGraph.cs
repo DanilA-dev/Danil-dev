79458286d5a61dd1b690df84a762669dd970f992
DanilA-dev <78906212+DanilA-dev@users.noreply.github.com>
2026-01-12 01:22:25 +0300

    .

--- Scripts/Runtime/Systems/AnimatorView/AnimationPlayable/PlayableMixers/AnimationPlayableGraph.cs
+++ Scripts/Runtime/Systems/AnimatorView/AnimationPlayable/PlayableMixers/AnimationPlayableGraph.cs
@@ -10,7 +10,7 @@ namespace D_Dev.AnimatorView.AnimationPlayableHandler
         #region Fields
 
         [SerializeField] private Animator _animator;
-        [SerializeField, Min(1)] private int _layerCount;
+        [SerializeField, Min(1)] private int _layerCount = 1;
 
         private AnimationPlayableOutput _animationPlayableOutput;
             
@@ -21,7 +21,6 @@ namespace D_Dev.AnimatorView.AnimationPlayableHandler
         public PlayableGraph PlayableGraph { get; private set; }
         public AnimationLayerMixerPlayable RootLayerMixer { get; private set; }
         public Animator Animator => _animator;
-        
         public bool IsPlayableGraphInitialized { get; private set; }
 
         #endregion
