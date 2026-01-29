20548d696a5ce11273178ca6b6363802cce656c5
DanilA-dev <78906212+DanilA-dev@users.noreply.github.com>
2026-01-30 01:13:51 +0300

    .

---
+++ Scripts/Runtime/Systems/AnimatorView/Extensions/AnimationClipConfigEntityVariable.cs
@@ -0,0 +1,26 @@
+﻿using D_Dev.EntityVariable;
+using D_Dev.ScriptableVaiables;
+
+namespace D_Dev.AnimatorView.Extensions
+{
+    [System.Serializable]
+    public class AnimationClipConfigEntityVariable : EntityVariable<AnimationClipConfig>
+    {
+        #region Constructors
+
+        public AnimationClipConfigEntityVariable() {}
+        
+        public AnimationClipConfigEntityVariable(StringScriptableVariable id, AnimationClipConfig value) : base(id, value) {}
+
+        #endregion
+        
+        #region Overrides
+        
+        public override BaseEntityVariable Clone()
+        {
+            return new AnimationClipConfigEntityVariable(_variableID, _value);
+        }
+        
+        #endregion
+    }
+}
