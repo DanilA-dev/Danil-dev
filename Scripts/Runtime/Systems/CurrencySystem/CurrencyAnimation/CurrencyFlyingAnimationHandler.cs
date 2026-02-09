faaad286e273efaa25eff7e511c5249b9d659258
DanilA-dev <78906212+DanilA-dev@users.noreply.github.com>
2026-02-10 02:47:20 +0300

    .

---
+++ Scripts/Runtime/Systems/CurrencySystem/CurrencyAnimation/CurrencyFlyingAnimationHandler.cs
@@ -0,0 +1,162 @@
+﻿using Cysharp.Threading.Tasks;
+using D_Dev.CustomEventManager;
+using D_Dev.PolymorphicValueSystem;
+using DG.Tweening;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace D_Dev.CurrencySystem.Extensions
+{
+    public class CurrencyFlyingAnimationHandler : MonoBehaviour
+    {
+        #region Classes
+
+        [System.Serializable]
+        public class FlyingCurrencyAnimation
+        {
+            #region Fields
+
+            [Title("General")]
+            [SerializeField] private GameObject _currencyItemPrefab;
+            [SerializeField] private int _maxAmount;
+            [SerializeField] private Vector2 _localScale = new(1, 1);
+
+            [Title("Offsets")] 
+            [SerializeField] private Vector2 _fromOffset;
+            [SerializeField] private Vector2 _toOffset;
+            
+            [Title("Canvas")]
+            [SerializeReference] private PolymorphicValue<Canvas> _canvas;
+
+            [Title("Tween Settings")] 
+            [SerializeField] private float _delayBetweenSpawn;
+            [SerializeField] private float _moveTime;
+            [SerializeField] private Ease _ease;
+            [SerializeField] private bool _ignoreTimeScale;
+
+            [FoldoutGroup("Events")] 
+            public UnityEvent OnSingleAnimationEnd;
+            [FoldoutGroup("Events")] 
+            public UnityEvent OnAllAnimationEnd;
+
+            #endregion
+
+            #region Properties
+
+            public GameObject CurrencyItemPrefab => _currencyItemPrefab;
+
+            public PolymorphicValue<Canvas> Canvas => _canvas;
+
+            public float MoveTime => _moveTime;
+
+            public Ease Ease => _ease;
+
+            public bool IgnoreTimeScale => _ignoreTimeScale;
+
+            public int MaxAmount => _maxAmount;
+
+            public Vector2 LocalScale
+            {
+                get => _localScale;
+                set => _localScale = value;
+            }
+
+            public Vector2 FromOffset => _fromOffset;
+
+            public Vector2 ToOffset => _toOffset;
+
+            public float DelayBetweenSpawn => _delayBetweenSpawn;
+
+            #endregion
+        }
+
+        #endregion
+        
+        #region Fields
+
+        [Title("Currency")] 
+        [SerializeReference] private PolymorphicValue<string> _flyingAnimationEventName = new StringConstantValue();
+
+        [Title("Flying Animations")] 
+        [SerializeField] private FlyingCurrencyAnimation _flyingCurrencyAnimation;
+
+        private Camera _camera;
+        
+        #endregion
+
+        #region Monobehaviour
+
+        private void Awake()
+        {
+            _camera = Camera.main;
+        }
+
+        private void OnEnable()
+        {
+            EventManager.AddListener<Transform, Transform, long>(_flyingAnimationEventName.Value, OnCurrencyUpdate);
+        }
+
+        private void OnDisable()
+        {
+            EventManager.RemoveListener<Transform, Transform, long>(_flyingAnimationEventName.Value, OnCurrencyUpdate);
+        }
+
+
+        #endregion
+
+        #region Listeners
+
+        private void OnCurrencyUpdate(Transform from, Transform to, long amount)
+        {
+            StartFlyingCurrencySequence(from, to, amount);
+        }
+
+        #endregion
+
+        #region Private
+
+        private async void StartFlyingCurrencySequence(Transform from, Transform to, long amount)
+        {
+            int createdAmount = Mathf.Min((int)amount, _flyingCurrencyAnimation.MaxAmount);
+            var canvasRect = _flyingCurrencyAnimation.Canvas.Value.GetComponent<RectTransform>();
+
+            Vector3 worldPos = from.position;
+            Vector2 screenPos = _camera.WorldToScreenPoint(worldPos);
+            
+            if (RectTransformUtility.ScreenPointToLocalPointInRectangle(
+                    canvasRect,
+                    screenPos,
+                    null,
+                    out Vector2 fromLocalPos))
+            {
+                for (int i = 0; i < createdAmount; i++)
+                {
+                    RectTransform currencyItem = Instantiate(
+                        _flyingCurrencyAnimation.CurrencyItemPrefab.GetComponent<RectTransform>(),
+                        canvasRect,
+                        false);
+
+                    var fromAnchoredPos = fromLocalPos + _flyingCurrencyAnimation.FromOffset;
+                    currencyItem.anchoredPosition = fromAnchoredPos;
+                    currencyItem.localScale = _flyingCurrencyAnimation.LocalScale;
+
+                    currencyItem.DOMove(to.position, _flyingCurrencyAnimation.MoveTime)
+                        .SetEase(_flyingCurrencyAnimation.Ease)
+                        .SetUpdate(_flyingCurrencyAnimation.IgnoreTimeScale)
+                        .OnComplete(() =>
+                        {
+                            _flyingCurrencyAnimation.OnSingleAnimationEnd?.Invoke();
+                            Destroy(currencyItem.gameObject);
+                        })
+                        .SetAutoKill();
+
+                    await UniTask.Delay((int)_flyingCurrencyAnimation.DelayBetweenSpawn * 1000, DelayType.Realtime);
+                }
+                _flyingCurrencyAnimation.OnAllAnimationEnd?.Invoke();
+            }
+        }
+
+        #endregion
+    }
+}
--- Scripts/Runtime/Systems/CurrencySystem/CurrencyInfo/CurrencyInfoSetter.cs
+++ Scripts/Runtime/Systems/CurrencySystem/CurrencyInfo/CurrencyInfoSetter.cs
@@ -1,8 +1,5 @@
-using System.Collections;
+using D_Dev.CustomEventManager;
 using D_Dev.PolymorphicValueSystem;
-using D_Dev.PositionRotationConfig;
-using D_Dev.TweenAnimations.Types;
-using DG.Tweening;
 using Sirenix.OdinInspector;
 using UnityEngine;
 using UnityEngine.Events;
@@ -11,30 +8,6 @@ namespace D_Dev.CurrencySystem
 {
     public class CurrencyInfoSetter : MonoBehaviour
     {
-        #region Classes
-
-        [System.Serializable]
-        public class FlyingCurrencyAnimation
-        {
-            public GameObject CurrencyItemPrefab;
-            [Space]
-            public long MaxCurrencyToSpawn;
-
-            [Space]
-            [SerializeReference] public BasePositionSettings DestinationPosition;
-            [SerializeReference] public BasePositionSettings SpawnPosition;
-            [SerializeReference] public PolymorphicValue<Canvas> Canvas = new CanvasConstantValue();
-            public Vector2 SpawnOffset;
-            public Vector2 DestinationOffset;
-            [Space]
-            public float DelayBetweenSpawns;
-            public MoveAnimationTween AnimationTween;
-            [Space]
-            public UnityEvent OnAllAnimationsComplete;
-        }
-
-        #endregion
-        
         #region Enums
 
         public enum StartCurrencySetAction
@@ -53,10 +26,13 @@ namespace D_Dev.CurrencySystem
         [SerializeField] private bool _setOnEnable;
         [ShowIf("_setOnEnable")]
         [SerializeField] private StartCurrencySetAction _startCurrencySetAction;
-        [FoldoutGroup("Flying Animation")] 
-        [SerializeField] private bool _animateFlyingCurrency;
-        [ShowIf(nameof(_animateFlyingCurrency))]
-        [SerializeField] private FlyingCurrencyAnimation _flyingCurrencyAnimation;
+        [SerializeField] private bool _useFlyingAnimation;
+        [ShowIf(nameof(_useFlyingAnimation))] 
+        [SerializeReference] private PolymorphicValue<string> _flyingAnimationEventName = new StringConstantValue();
+        [ShowIf(nameof(_useFlyingAnimation))]
+        [SerializeReference] private PolymorphicValue<Transform> _from;
+        [ShowIf(nameof(_useFlyingAnimation))]
+        [SerializeReference] private PolymorphicValue<Transform> _to;
         [Space]
         [FoldoutGroup("Deposit Events")]
         public UnityEvent OnDepositSuccess;
@@ -96,7 +72,8 @@ namespace D_Dev.CurrencySystem
             if (_currencyInfo != null && _currencyInfo.Value.Currency.TryDeposit(amount))
             {
                 OnDepositSuccess?.Invoke();
-                TryAnimateFlyingCurrency(amount);
+                if(_useFlyingAnimation)
+                    EventManager.Invoke(_flyingAnimationEventName.Value, _from.Value, _to.Value, amount);
                 return true;
             }
             OnDepositFailed?.Invoke();
@@ -108,7 +85,8 @@ namespace D_Dev.CurrencySystem
             if (_currencyInfo != null && _currencyInfo.Value.Currency.TryWithdraw(amount))
             {
                 OnWithdrawSuccess?.Invoke();
-                TryAnimateFlyingCurrency(amount);
+                if(_useFlyingAnimation)
+                    EventManager.Invoke(_flyingAnimationEventName.Value, _from.Value, _to.Value, amount);
                 return true;
             }
             OnWithdrawFailed?.Invoke();
@@ -149,136 +127,7 @@ namespace D_Dev.CurrencySystem
                 }
             }
         }
-        
-        private void TryAnimateFlyingCurrency(long amount)
-        {
-            if(!_animateFlyingCurrency)
-                return;
-
-            StartCoroutine(FlyingCurrencyCoroutine(amount));
-        }
-
-        private IEnumerator FlyingCurrencyCoroutine(long amount)
-        {
-            var config = _flyingCurrencyAnimation;
-            
-            if(config.CurrencyItemPrefab == null)
-            {
-                Debug.LogError("[CurrencyInfoSetter] CurrencyItemPrefab is null");
-                yield break;
-            }
-
-            if(config.CurrencyItemPrefab.GetComponent<RectTransform>() == null)
-            {
-                Debug.LogError("[CurrencyInfoSetter] CurrencyItemPrefab must have RectTransform component");
-                yield break;
-            }
-            
-            Canvas canvas = config.Canvas?.Value;
-            if(canvas == null)
-            {
-                Debug.LogError("[CurrencyInfoSetter] Canvas is null");
-                yield break;
-            }
-
-            Camera mainCamera = Camera.main;
-            if(mainCamera == null)
-            {
-                Debug.LogError("[CurrencyInfoSetter] Camera.main is null");
-                yield break;
-            }
-
-            RectTransform canvasRect = canvas.GetComponent<RectTransform>();
-            if(canvasRect == null)
-            {
-                Debug.LogError("[CurrencyInfoSetter] Canvas has no RectTransform");
-                yield break;
-            }
-
-            Camera uiCamera = canvas.renderMode == RenderMode.ScreenSpaceOverlay ? null : canvas.worldCamera;
-
-            int spawnCount = (int)Mathf.Min(amount, config.MaxCurrencyToSpawn);
-            int completedAnimations = 0;
-            int activeAnimations = 0;
-
-            for(int i = 0; i < spawnCount; i++)
-            {
-                Vector3 spawnWorldPos = config.SpawnPosition?.GetPosition() ?? Vector3.zero;
-                Vector3 destWorldPos = config.DestinationPosition?.GetPosition() ?? Vector3.zero;
-
-                spawnWorldPos += new Vector3(config.SpawnOffset.x, config.SpawnOffset.y, 0);
-                destWorldPos += new Vector3(config.DestinationOffset.x, config.DestinationOffset.y, 0);
-
-                Vector2 spawnScreenPos = mainCamera.WorldToScreenPoint(spawnWorldPos);
-                Vector2 destScreenPos = mainCamera.WorldToScreenPoint(destWorldPos);
-
-                if(!RectTransformUtility.ScreenPointToLocalPointInRectangle(
-                    canvasRect, spawnScreenPos, uiCamera, out Vector2 spawnLocalPos))
-                {
-                    spawnLocalPos = Vector2.zero;
-                }
-
-                if(!RectTransformUtility.ScreenPointToLocalPointInRectangle(
-                    canvasRect, destScreenPos, uiCamera, out Vector2 destLocalPos))
-                {
-                    destLocalPos = Vector2.zero;
-                }
-
-                GameObject currencyObj = Instantiate(config.CurrencyItemPrefab, canvas.transform);
-                currencyObj.transform.SetAsLastSibling();
-
-                RectTransform currencyRect = currencyObj.GetComponent<RectTransform>();
-                currencyRect.anchoredPosition = spawnLocalPos;
-
-                if(config.AnimationTween != null)
-                {
-                    activeAnimations++;
-                    MoveAnimationTween tween = config.AnimationTween;
-                    
-                    if(currencyObj.TryGetComponent<Transform>(out Transform currencyTransform))
-                    {
-                        tween.MovedObject = currencyTransform;
-                    }
-
-                    if(tween.MoveType == MoveAnimationTween.MoveObjectType.Vector)
-                    {
-                        tween.PositionStart = spawnLocalPos;
-                        tween.PositionEnd = destLocalPos;
-                        tween.UseInitialPositionAsStart = false;
-                    }
-                    else if(tween.MoveType == MoveAnimationTween.MoveObjectType.Transform)
-                    {
-                        tween.UseInitialPositionAsStart = true;
-                    }
-
-                    var tweener = tween.Play();
-                    if(tweener != null)
-                    {
-                        tweener.OnComplete(() =>
-                        {
-                            Destroy(currencyObj);
-                            completedAnimations++;
-                            
-                            if(completedAnimations >= activeAnimations && config.OnAllAnimationsComplete != null)
-                            {
-                                config.OnAllAnimationsComplete.Invoke();
-                            }
-                        });
-                    }
-                }
-                else
-                {
-                    Destroy(currencyObj);
-                }
-
-                if(config.DelayBetweenSpawns > 0 && i < spawnCount - 1)
-                {
-                    yield return new WaitForSeconds(config.DelayBetweenSpawns);
-                }
-            }
-        }
 
         #endregion
-        
     }
 }
