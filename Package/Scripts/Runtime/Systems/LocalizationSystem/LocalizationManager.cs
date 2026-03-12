b22003a81a11eb1c2cf363a5d39a9cdde62b279e
DanilA-dev <78906212+DanilA-dev@users.noreply.github.com>
2026-03-12 18:09:58 +0300

    .

---
+++ Package/Scripts/Runtime/Systems/LocalizationSystem/LocalizationManager.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.Localization;
+using UnityEngine.Localization.Settings;
+
+namespace D_Dev.LocalizationSystem
+{
+    public class LocalizationManager : MonoBehaviour
+    {
+        #region Fields
+
+        private const string DefaultLocaleCode = "en";
+
+        [SerializeField] private bool _autoAssignLocale;
+        [SerializeReference] private ILocaleAutoAssigner _localeAutoAssigner;
+
+        #endregion
+
+        #region MonoBehaviour
+
+        private void Awake()
+        {
+            if (_autoAssignLocale)
+                TryAssignAutoLocale();
+        }
+
+        #endregion
+
+        #region Public
+
+        public static void ChangeLocale(Locale locale)
+        {
+            if (locale != null)
+                LocalizationSettings.SelectedLocale = locale;
+        }
+
+        #endregion
+
+        #region Private
+
+        private void TryAssignAutoLocale()
+        {
+            Locale locale = _localeAutoAssigner?.GetLocale() ?? GetDefaultLocale();
+            
+            if (locale != null)
+                LocalizationSettings.SelectedLocale = locale;
+        }
+
+        private static Locale GetDefaultLocale()
+        {
+            return LocalizationSettings.AvailableLocales.GetLocale(DefaultLocaleCode);
+        }
+
+        #endregion
+    }
+}
