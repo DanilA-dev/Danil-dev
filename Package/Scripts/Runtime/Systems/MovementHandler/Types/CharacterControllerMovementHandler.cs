1e7c35e315c92fca3b5189db2d5e3f17dbace85f
DanilA-dev <78906212+DanilA-dev@users.noreply.github.com>
2026-03-18 12:45:21 +0300

    .

--- Package/Scripts/Runtime/Systems/MovementHandler/Types/CharacterControllerMovementHandler.cs
+++ Package/Scripts/Runtime/Systems/MovementHandler/Types/CharacterControllerMovementHandler.cs
@@ -1,3 +1,4 @@
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace D_Dev.MovementHandler
@@ -8,7 +9,12 @@ namespace D_Dev.MovementHandler
         #region Fields
 
         [SerializeField] private CharacterController _characterController;
+        [SerializeField] private bool _useGravity = true;
+        [ShowIf(nameof(_useGravity))]
+        [SerializeField] private float _gravityModifier = 1f;
+
         private Vector3 _currentVelocity;
+        private float _verticalVelocity;
 
         #endregion
 
@@ -28,17 +34,38 @@ namespace D_Dev.MovementHandler
             else
                 _currentVelocity = Vector3.Lerp(_currentVelocity, Vector3.zero, Time.deltaTime * Acceleration);
 
-            _characterController.Move(_currentVelocity * Time.deltaTime);
+            if (_useGravity)
+            {
+                if (_characterController.isGrounded)
+                    _verticalVelocity = -0.5f;
+                else
+                    _verticalVelocity += Physics.gravity.y * _gravityModifier * Time.deltaTime;
+            }
+            else
+            {
+                _verticalVelocity = 0f;
+            }
+
+            Vector3 motion = _currentVelocity + Vector3.up * _verticalVelocity;
+            _characterController.Move(motion * Time.deltaTime);
         }
 
         public override void StopMovement()
         {
             _currentVelocity = Vector3.zero;
+            _verticalVelocity = 0f;
             Direction = Vector3.zero;
         }
 
         public override float GetVelocity() => _currentVelocity.magnitude;
         public override bool IsMoving() => _currentVelocity.magnitude > 0.1f;
+
+        #endregion
+
+        #region Public
+
+        public void SetGravity(bool enabled) => _useGravity = enabled;
+        public void SetGravityModifier(float modifier) => _gravityModifier = modifier;
 
         #endregion
     }
